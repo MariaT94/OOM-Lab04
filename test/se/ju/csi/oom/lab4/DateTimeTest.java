1eafbc7d1bca13688ad96205305552265a977abb
MariaT94 <44158339+MariaT94@users.noreply.github.com>
2019-03-30 15:36:38 +0100

    Add files via upload

---
+++ test/se/ju/csi/oom/lab4/DateTimeTest.java
@@ -0,0 +1,32 @@
+package se.ju.csi.oom.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTimeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		String testString = "1994-03-09 11:00"; 
+		DateTime mariasBirthday = new DateTime(1994,3,9,11,0); 
+	 
+		assertEquals("Fail, not equals", testString, mariasBirthday.toString()); 
+
+	}
+
+	@Test
+	public void testDateTimeString() {
+		String testString = "1994-03-09 11:00";   
+		DateTime mariasBirthday = new DateTime(testString); 
+		
+		assertEquals("Fail, not equals", testString, mariasBirthday.toString()); 
+
+	}
+
+}
